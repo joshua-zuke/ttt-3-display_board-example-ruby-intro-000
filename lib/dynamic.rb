7bee62431159bb6d3e0dc24e933a9fdf0f0b3748
Joshua Zuke <zuke.joshua@gmail.com>
2021-02-11 16:41:24 +0000

    Automatically backed up by Learn

--- lib/dynamic.rb
+++ lib/dynamic.rb
@@ -3,4 +3,4 @@ def greeting(name, language, x)
       puts "Hello, #{name}! We heard you are a great #{language} speaker."
     end
   end
-  greeting("Josh", "Spanish", 3)
+  greeting "Josh", "Spanish", 3
